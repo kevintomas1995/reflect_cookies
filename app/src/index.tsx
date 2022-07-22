fcd175f0aaf5897c886fc9fcea33413959e1c952
ktomas <kevin.tomas@axelspringer.com>
2022-07-22 11:49:03 +0200

    add frontend

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <CookiesProvider>
+  <React.StrictMode>
     <App />
-  </CookiesProvider>
+  </React.StrictMode>
 );
